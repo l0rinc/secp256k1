ef2cd1c56ba92c31b535282eedc8bc4859e96ec9
Lőrinc <pap.lorinc@gmail.com>
2026-05-29 13:04:42 +0200

    field: force-inline 5x52 zero check

--- src/field_5x52_impl.h
+++ src/field_5x52_impl.h
@@ -134,7 +134,7 @@ SECP256K1_ALWAYS_INLINE static void secp256k1_fe_impl_normalize_var(secp256k1_fe
     r->n[0] = t0; r->n[1] = t1; r->n[2] = t2; r->n[3] = t3; r->n[4] = t4;
 }
 
-static int secp256k1_fe_impl_normalizes_to_zero(const secp256k1_fe *r) {
+SECP256K1_ALWAYS_INLINE static int secp256k1_fe_impl_normalizes_to_zero(const secp256k1_fe *r) {
     uint64_t t0 = r->n[0], t1 = r->n[1], t2 = r->n[2], t3 = r->n[3], t4 = r->n[4];
 
     /* z0 tracks a possible raw value of 0, z1 tracks a possible raw value of P */
