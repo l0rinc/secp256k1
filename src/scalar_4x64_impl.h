f6fd56c4bef4476a9cf27300fb22f9f459e28816
Lőrinc <pap.lorinc@gmail.com>
2026-05-29 13:08:36 +0200

    scalar: force-inline bit reader limb32

--- src/scalar_4x64_impl.h
+++ src/scalar_4x64_impl.h
@@ -38,7 +38,7 @@ SECP256K1_INLINE static void secp256k1_scalar_set_int(secp256k1_scalar *r, unsig
     SECP256K1_SCALAR_VERIFY(r);
 }
 
-SECP256K1_INLINE static uint32_t secp256k1_scalar_get_bits_limb32(const secp256k1_scalar *a, unsigned int offset, unsigned int count) {
+SECP256K1_ALWAYS_INLINE static uint32_t secp256k1_scalar_get_bits_limb32(const secp256k1_scalar *a, unsigned int offset, unsigned int count) {
     SECP256K1_SCALAR_VERIFY(a);
     VERIFY_CHECK(count > 0 && count <= 32);
     VERIFY_CHECK((offset + count - 1) >> 6 == offset >> 6);
