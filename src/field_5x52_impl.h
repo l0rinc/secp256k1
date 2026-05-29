aebc8f2a8a36713259faa4b436d32f7e81a65756
Lőrinc <pap.lorinc@gmail.com>
2026-05-29 13:05:44 +0200

    field: force-inline 5x52 is-square

--- src/field_5x52_impl.h
+++ src/field_5x52_impl.h
@@ -498,7 +498,7 @@ static void secp256k1_fe_impl_inv_var(secp256k1_fe *r, const secp256k1_fe *x) {
     secp256k1_fe_from_signed62(r, &s);
 }
 
-static int secp256k1_fe_impl_is_square_var(const secp256k1_fe *x) {
+SECP256K1_ALWAYS_INLINE static int secp256k1_fe_impl_is_square_var(const secp256k1_fe *x) {
     secp256k1_fe tmp;
     secp256k1_modinv64_signed62 s;
     int jac, ret;
